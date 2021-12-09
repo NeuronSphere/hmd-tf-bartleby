18d64e135ea4de21dd96f58a9381ab505360327e
Kate Walls <kate.walls@hmdlabs.io>
2021-12-09 08:06:36 -0700

    test: add placeholder unit tests

---
+++ src/python/test/bartleby_test.py
@@ -0,0 +1,15 @@
+import os
+from hmd_tf_bartleby.hmd_tf_bartleby import entry_point
+from unittest.mock import MagicMock, patch
+from testfixtures.popen import MockPopen
+
+
+class TestBartleby:
+    def test_html_input(self):
+        pass
+
+    def test_pdf_input(self):
+        pass
+
+    def test_default_input(self):
+        pass
